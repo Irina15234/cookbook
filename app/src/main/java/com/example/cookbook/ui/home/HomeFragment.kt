c38031f2a306a8250dbfd636f4fbf9892b3522be
Irina15234 <irina15234@yandex.ru>
2024-08-05 17:57:47 +0300

    add categories grid

--- app/src/main/java/com/example/cookbook/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cookbook/ui/home/HomeFragment.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.cookbook.CategoryGridAdapter
 import com.example.cookbook.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -28,10 +28,9 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        val categoryAdapter = CategoryGridAdapter(homeViewModel.categories, requireContext())
+        binding.categoriesGrid.adapter = categoryAdapter
+
         return root
     }
 
--- app/src/main/java/com/example/cookbook/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/cookbook/ui/home/HomeViewModel.kt
@@ -1,13 +1,16 @@
 package com.example.cookbook.ui.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.cookbook.R
 
 class HomeViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
+    val categories: List<FoodCategory> = listOf(
+        FoodCategory("soup", "Супы", R.drawable.soup),
+        FoodCategory("salad", "Салаты", R.drawable.salad),
+        FoodCategory("appetizer", "Закуски", R.drawable.appetizer),
+        FoodCategory("hot", "Горячее", 0),
+        FoodCategory("dessert", "Десерты", 0),
+    );
 }
+
+data class FoodCategory(val id: String, val name: String, val img: Int)
